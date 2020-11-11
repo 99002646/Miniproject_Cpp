9fd68b1e226500f5baa2240b73d281e6d5415550
99002646 <71603593+99002646@users.noreply.github.com>
2020-11-11 09:51:22 +0530

    Update Patient.cpp

--- MyProject/Patient.cpp
+++ MyProject/Patient.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #include<string>
 #include <sstream>
 
-		//Patient::Patient():Appointment(),shelf(0){ }
+		Patient::Patient():Appointment(),shelf(0){ }
  	 	Patient::Patient(std::string f_nbrhd,int f_pntid,int f_appid,int f_gender,int f_age,double f_sdldday,double f_appday,int f_hiper,int f_diabetes,int f_alcohal,int f_handcap,int ff_show):Appointment( f_nbrhd, f_pntid, f_appid, f_gender, f_age, f_sdldday, f_appday, f_hiper, f_diabetes, f_alcohal, f_handcap,ff_show){}
  	 	
    int Patient::getpntid()
